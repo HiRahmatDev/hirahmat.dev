7428c1ad4885c3837c62c19d544fb0361b2f5687
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-10-13 21:40:04 +0700

    feat: add sendEmail handler in `CtaButton.tsx`

--- app/components/CtaButton.tsx
+++ app/components/CtaButton.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import { Mail } from "lucide-react";
 
 export function CtaButton() {
+  const sendEmail = () => {
+    const email = "hirahmat.dev@gmail.com";
+    const subject = encodeURIComponent("Hi Rahmat!");
+    const body = encodeURIComponent(
+      "Halo Rahmat,\n\nSaya tertarik dengan portfoliomu di HiRahmat.Dev.\nBoleh ngobrol sebentar?"
+    );
+
+    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+  };
+
   return (
-    <button type="button" className="cta-button">
+    <button type="button" className="cta-button" onClick={sendEmail}>
       <Mail />
       Kirim Email
     </button>
