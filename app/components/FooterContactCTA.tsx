3b0b6b2fa7eb1586ebf90771775727f52e7cb528
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-10-13 22:42:07 +0700

    feat: add background accent in `FooterContactCTA.tsx`

--- app/components/FooterContactCTA.tsx
+++ app/components/FooterContactCTA.tsx
@@ -1,10 +1,13 @@
-import { EMAIL_HREF } from "./constants";
 import { Mail } from "lucide-react";
+import Image from "next/image";
+
+import { EMAIL_HREF } from "./constants";
+import backgroundFooterContactCtaPng from "../../public/images/background-footer-contact-cta.png";
 
 export function FooterContactCTA() {
   return (
     <div className="container py-6">
-      <div className="p-5 bg-accent text-white rounded-[20px] flex gap-3">
+      <div className="p-5 bg-accent text-white rounded-[20px] flex gap-3 relative overflow-hidden">
         <Mail className="shrink-0" />
         <div className="space-y-1">
           <h2 className="text-xl/[24px] font-semibold tracking-[-0.35px]">
@@ -19,6 +22,13 @@ export function FooterContactCTA() {
             </a>
           </p>
         </div>
+        <Image
+          src={backgroundFooterContactCtaPng}
+          alt="background footer contact cta accent"
+          placeholder="blur"
+          height={128}
+          className="absolute top-[50%] right-0 translate-y-[-50%]"
+        />
       </div>
     </div>
   );
