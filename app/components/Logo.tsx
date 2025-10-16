699a48e4d81c7858a6b9f4ebdd20d1f0a7eb99b7
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-10-16 13:59:05 +0700

    style: enhance logo's style

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Logo() {
   return (
-    <Link className="font-mono text-xl/[40px] font-bold tracking-[-1.5px] animate-hover" href="/">
+    <Link className="font-mono text-xl/[40px] font-bold tracking-[-1.5px] animate-hover -rotate-1" href="/">
       {"<"}
       HiRahmat.<span className="text-accent">Dev</span>
       {"/>"}
