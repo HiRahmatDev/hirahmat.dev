b27562796a990fc6f91b3d16177e0bc860a34e38
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-10-17 16:38:42 +0700

    style: add animation in `SelectedProjectsSection.tsx`

---
+++ app/components/sections/SelectedProjectsSection/SelectedProjectCards.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { motion, stagger } from "motion/react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { SelectedProject } from "@/app/services/notion";
+
+type SelectedProjectCardsProps = {
+  selectedProjects: SelectedProject[];
+};
+
+export function SelectedProjectCards({
+  selectedProjects,
+}: SelectedProjectCardsProps) {
+  const container = {
+    hidden: { opacity: 0, y: 20 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delayChildren: stagger(0.3, { startDelay: 0.3 }),
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 0 },
+  };
+
+  return (
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+      className="flex gap-5 [&>div]:shrink-0"
+    >
+      {(selectedProjects || []).map((project) => (
+        <motion.div
+          key={project.slug}
+          variants={item}
+          transition={{ duration: 1 }}
+          className="h-full"
+        >
+          <div className="rounded-2xl bg-accent w-[439px] h-[217px] shadow-xl flex gap-3 text-white overflow-hidden">
+            <div className="w-full py-5 pl-5 flex flex-col justify-between gap-3 [&>*]:max-w-fit">
+              <div className="space-y-3">
+                <h3 className="font-semibold text-xl/[24px] tracking-[-0.35px]">
+                  {project.title}
+                </h3>
+                <p className="text-sm/[20px] max-w-[156px]">{project.desc}</p>
+              </div>
+              <Link
+                href={`/article/${project.slug}`}
+                className="white-solid-button animate-hover"
+              >
+                Lihat Jurnal
+              </Link>
+            </div>
+            <div className="shrink-0 min-w-[104px] py-5 pr-5 relative">
+              <div className="absolute left-[-130px] bottom-[-48px] blur-[1px] opacity-60">
+                <div className="relative w-[378px] h-[187px] shadow-2xl rounded-md -rotate-2 overflow-hidden">
+                  {project.img && (
+                    <Image
+                      src={project.img}
+                      alt=""
+                      fill
+                      quality={0}
+                      sizes="(max-width: 768px) 80vw, (max-width: 1200px) 30vw, 10vw"
+                      className="object-cover right-0"
+                      unoptimized
+                    />
+                  )}
+                </div>
+              </div>
+              <div className="absolute left-[-120px] bottom-[-42px]">
+                <div className="relative w-[378px] h-[187px] shadow-2xl rounded-md rotate-2 overflow-hidden">
+                  {project.img && (
+                    <Image
+                      src={project.img}
+                      alt=""
+                      fill
+                      sizes="(max-width: 768px) 80vw, (max-width: 1200px) 30vw, 10vw"
+                      className="object-cover right-0"
+                      unoptimized
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      ))}
+    </motion.div>
+  );
+}
