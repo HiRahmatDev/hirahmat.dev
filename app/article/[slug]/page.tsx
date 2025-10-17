83b6611496a7b0e778692677f193872057c19338
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-10-17 13:44:29 +0700

    style: update style in article detail page for consistency

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -52,9 +52,9 @@ export default async function ArticleDetailPage({
   const page = await fetchPageByBlockId(blockMetadata.blockId);
 
   return (
-    <div className="container-for-reading">
-      <article className="space-y-11">
-        <section className="pt-8 pb-11 space-y-6">
+    <article className="space-y-11">
+      <section className="pt-6 space-y-6">
+        <div className="container-for-reading">
           <section className="space-y-1 text-foreground/60">
             <p className="text-lg/[24px] font-semibold tracking-[-0.35]">
               {blockMetadata.category || "-"}
@@ -63,12 +63,18 @@ export default async function ArticleDetailPage({
               <em>{formatPublishedDate(blockMetadata.publishedOn) || "-"}</em>
             </p>
           </section>
+        </div>
+        <div className="container-for-reading space-y-6">
           <header className="space-y-1">
             <h1 className="text-4xl/[44px] tracking-[-1px] font-bold">
               {blockMetadata.title}
             </h1>
-            <p className="text-lg/[32px] tracking-[-0.35px]">{blockMetadata.desc}</p>
+            <p className="text-lg/[32px] tracking-[-0.35px]">
+              {blockMetadata.desc}
+            </p>
           </header>
+        </div>
+        <div className="container">
           {blockMetadata.img ? (
             <figure>
               <Image
@@ -81,11 +87,13 @@ export default async function ArticleDetailPage({
               />
             </figure>
           ) : null}
-        </section>
-        <section className="pb-24">
+        </div>
+      </section>
+      <section className="pb-12">
+        <div className="container-for-reading">
           <NotionRenderer page={page} />
-        </section>
-      </article>
-    </div>
+        </div>
+      </section>
+    </article>
   );
 }
