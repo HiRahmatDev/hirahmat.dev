d403e1d0d2f33c762c06db3e87ed5caad4cde0a7
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-10-19 09:50:13 +0700

    fix: serious accebility from vercel

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 
+import { ContactCTA } from "@/app/components/ContactCTA";
 import {
   fetchArticleByBlockId,
   fetchArticleMetadataBySlug,
@@ -50,48 +51,51 @@ export default async function ArticleDetailPage({
   const article = await fetchArticleByBlockId(blockMetadata.blockId);
 
   return (
-    <article className="space-y-11">
-      <section className="pt-6 space-y-6">
-        <div className="container-for-reading">
-          <section className="space-y-1 text-foreground/60">
-            <p className="text-lg/[24px] font-semibold tracking-[-0.35]">
-              {blockMetadata.category || "-"}
-            </p>
-            <p className="text-xs/[16px]">
-              <em>{formatPublishedDate(blockMetadata.publishedOn) || "-"}</em>
-            </p>
-          </section>
-        </div>
-        <div className="container-for-reading space-y-6">
-          <header className="space-y-1">
-            <h1 className="text-4xl/[44px] tracking-[-1px] font-bold">
-              {blockMetadata.title}
-            </h1>
-            <p className="text-lg/[32px] tracking-[-0.35px]">
-              {blockMetadata.desc}
-            </p>
-          </header>
-        </div>
-        <div className="container">
-          {blockMetadata.cover ? (
-            <figure>
-              <Image
-                src={blockMetadata.cover}
-                alt="Screenshot Dasbor Operasional Kulina"
-                width={1280}
-                height={720}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 80"
-                className="object-cover top-0 right-0 left-0"
-              />
-            </figure>
-          ) : null}
-        </div>
-      </section>
-      <section className="pb-12">
-        <div className="container-for-reading">
-          <NotionRenderer listBlockChildren={article} />
-        </div>
-      </section>
-    </article>
+    <>
+      <article className="space-y-11">
+        <section className="pt-6 space-y-6">
+          <div className="container-for-reading">
+            <section className="space-y-1 text-foreground/60">
+              <p className="text-lg/[24px] font-semibold tracking-[-0.35]">
+                {blockMetadata.category || "-"}
+              </p>
+              <p className="text-xs/[16px]">
+                <em>{formatPublishedDate(blockMetadata.publishedOn) || "-"}</em>
+              </p>
+            </section>
+          </div>
+          <div className="container-for-reading space-y-6">
+            <header className="space-y-1">
+              <h1 className="text-4xl/[44px] tracking-[-1px] font-bold">
+                {blockMetadata.title}
+              </h1>
+              <p className="text-lg/[32px] tracking-[-0.35px]">
+                {blockMetadata.desc}
+              </p>
+            </header>
+          </div>
+          <div className="container">
+            {blockMetadata.cover ? (
+              <figure>
+                <Image
+                  src={blockMetadata.cover}
+                  alt="Screenshot Dasbor Operasional Kulina"
+                  width={1280}
+                  height={720}
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 80"
+                  className="object-cover top-0 right-0 left-0"
+                />
+              </figure>
+            ) : null}
+          </div>
+        </section>
+        <section className="pb-12">
+          <div className="container-for-reading">
+            <NotionRenderer listBlockChildren={article} />
+          </div>
+        </section>
+      </article>
+      <ContactCTA />
+    </>
   );
 }
