6e22ed356fa9afb1e6f4100fdb3d20564734211d
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-10-27 22:42:29 +0700

    refactor: add `CopyButton.tsx` and use it in `CodeBlock.tsx`
    - style: add flex header layout for code block

--- app/components/NotionRenderer/common/CodeBlock.tsx
+++ app/components/NotionRenderer/common/CodeBlock.tsx
@@ -1,14 +1,19 @@
 import { CodeBlockObjectResponse } from "@notionhq/client";
+
+import { CopyButton } from "../../CopyButton";
 import { highlightCode } from "@/app/lib/prism";
 
 export function CodeBlock({ block }: { block: CodeBlockObjectResponse }) {
-  const highlighted = highlightCode(
-    block.code.rich_text.map((rt) => rt.plain_text).join("") || "",
-    block.code.language
-  );
+  const codeBlock =
+    block.code.rich_text.map((rt) => rt.plain_text).join("") || "";
+  const highlighted = highlightCode(codeBlock, block.code.language);
 
   return (
     <pre className={`language-${block.code.language}`}>
+      <div className="code-block__header">
+        <span className="lang">{block.code.language}</span>
+        <CopyButton text={codeBlock} className="code-block__button-copy" />
+      </div>
       <code
         dangerouslySetInnerHTML={{ __html: highlighted }}
         className={`language-${block.code.language}`}
