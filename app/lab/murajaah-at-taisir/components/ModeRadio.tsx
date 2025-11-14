655d216e7265e92a1f172e91190ea118c8ea7c28
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-11-14 21:27:48 +0700

    style: replace radio button with icon in ModeRadio component

--- app/lab/murajaah-at-taisir/components/ModeRadio.tsx
+++ app/lab/murajaah-at-taisir/components/ModeRadio.tsx
@@ -2,16 +2,28 @@ import clsx from "clsx";
 
 import { MurajaahMode } from "../context/MurajaahContext";
 import { Label } from "./Label";
+import { Brain, Dumbbell, Lightbulb, LucideProps } from "lucide-react";
+import { ForwardRefExoticComponent, RefAttributes } from "react";
 
 type ModeRadioProps = {
   value: MurajaahMode;
   onChange: (mode: MurajaahMode) => void;
 };
 
-const RADIO_OPTIONS: { label: string; value: MurajaahMode }[] = [
-  { label: "Tadzkirah (dengan kata awal)", value: "TADZKIRAH" },
-  { label: "Dzikr (tanpa bantuan)", value: "DZIKR" },
-  { label: "Tadrib (latihan)", value: "TADRIB" },
+const RADIO_OPTIONS: {
+  label: string;
+  value: MurajaahMode;
+  icon: ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
+  >;
+}[] = [
+  {
+    label: "Tadzkirah (dengan kata awal)",
+    value: "TADZKIRAH",
+    icon: Lightbulb,
+  },
+  { label: "Dzikr (tanpa bantuan)", value: "DZIKR", icon: Brain },
+  { label: "Tadrib (latihan)", value: "TADRIB", icon: Dumbbell },
 ];
 
 export function ModeRadio({ value: valueProp, onChange }: ModeRadioProps) {
@@ -19,7 +31,7 @@ export function ModeRadio({ value: valueProp, onChange }: ModeRadioProps) {
     <div className="flex flex-col gap-1">
       <Label>Mode</Label>
       <div className="flex flex-col gap-2">
-        {RADIO_OPTIONS.map(({ label, value }) => {
+        {RADIO_OPTIONS.map(({ label, value, icon: Icon }) => {
           const isActive = value === valueProp;
 
           return (
@@ -29,23 +41,29 @@ export function ModeRadio({ value: valueProp, onChange }: ModeRadioProps) {
                 "flex items-center gap-2 text-base tracking-[-0.3px] border px-3 py-2 rounded-lg cursor-pointer",
                 value === "TADRIB"
                   ? clsx(
-                      "bg-calm/10 border-calm/20 text-text-calm",
-                      isActive && "border-calm/60"
+                      "bg-calm/10 text-text-calm",
+                      isActive
+                        ? "border-calm/60 [&>svg]:stroke-calm"
+                        : "border-calm/20 [&>svg]:stroke-blue-hover"
                     )
                   : clsx(
-                      "bg-accent/10 border-accent/20 text-text-accent",
-                      isActive && "border-accent/60"
+                      "bg-accent/10 text-text-accent",
+                      isActive
+                        ? "border-accent/60 [&>svg]:stroke-accent"
+                        : "border-accent/20 [&>svg]:stroke-accent-hover"
                     ),
                 isActive && "border-2 font-semibold"
               )}
             >
               <input
+                hidden
                 type="radio"
                 name="mode"
                 value={value}
                 checked={isActive}
                 onChange={() => onChange(value)}
               />
+              <Icon className="size-5" />
               {label}
             </label>
           );
