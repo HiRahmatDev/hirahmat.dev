3289903c7be8781901ec49ce05702f6eb5c219cd
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-11-15 17:29:03 +0700

    feat: animate bottom sheet in MenuButton

--- app/lab/murajaah-at-taisir/components/MenuButton.tsx
+++ app/lab/murajaah-at-taisir/components/MenuButton.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-aria-components";
 import { Menu } from "lucide-react";
 import { twMerge } from "tailwind-merge";
+import { useEffect, useState } from "react";
 
 import { AyahInputNumber } from "./AyahInputNumber";
 import { Label } from "./Label";
@@ -30,8 +31,53 @@ export function MenuButton({ className }: { className?: string }) {
     maxAyah,
   } = useMurajaahContext();
 
+  const [isOpen, setOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+    const modalOverlay = document.querySelector(".modal-overlay");
+    if (!modalOverlay) return;
+
+    if (isOpen) {
+      timeoutId = setTimeout(() => {
+        modalOverlay.classList.replace("overlay-enter-active", "overlay-enter");
+      }, 300);
+    } else {
+      modalOverlay.classList.replace("overlay-enter", "overlay-exit-active");
+    }
+    return () => clearTimeout(timeoutId);
+  }, [isOpen]);
+
+  useEffect(() => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+    const bottomSheet = document.querySelector(".bottom-sheet");
+    if (!bottomSheet) return;
+
+    if (isOpen) {
+      timeoutId = setTimeout(() => {
+        bottomSheet.classList.replace("slide-up-active", "slide-up");
+      }, 300);
+    } else {
+      bottomSheet.classList.replace("slide-up", "slide-down-active");
+    }
+    return () => clearTimeout(timeoutId);
+  }, [isOpen]);
+
+  const handleOpenChange = (open: boolean) => {
+    if (open) {
+      setOpen(true);
+      setIsVisible(true);
+    } else {
+      setOpen(false);
+      setTimeout(() => {
+        setIsVisible(false);
+      }, 300);
+    }
+  };
+
   return (
-    <DialogTrigger>
+    <DialogTrigger isOpen={isVisible} onOpenChange={handleOpenChange}>
       <Pressable>
         <button
           className={twMerge(
@@ -43,9 +89,12 @@ export function MenuButton({ className }: { className?: string }) {
           <Menu />
         </button>
       </Pressable>
-      <ModalOverlay isDismissable className="fixed inset-0 z-20 bg-black/30">
+      <ModalOverlay
+        isDismissable
+        className="fixed inset-0 z-20 modal-overlay overlay-enter-active"
+      >
         <Modal>
-          <Dialog className="bg-white fixed bottom-0 left-0 right-0 rounded-t-xl pb-8 pt-6">
+          <Dialog className="bg-white fixed bottom-0 left-0 right-0 rounded-t-xl pb-8 pt-6 bottom-sheet slide-up-active">
             <div className="mx-auto max-w-[420px] px-5">
               <div className="flex flex-col gap-5">
                 <div className="flex flex-col gap-1">
