dcd8d1014d887233dcdfebe3cbc601a606513d5c
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2025-11-16 11:19:33 +0700

    style: set max height for content of bottom sheet of MenuButton

--- app/lab/murajaah-at-taisir/components/MenuButton.tsx
+++ app/lab/murajaah-at-taisir/components/MenuButton.tsx
@@ -58,8 +58,10 @@ export function MenuButton({ className }: { className?: string }) {
             className="bg-white fixed bottom-0 left-0 right-0 rounded-t-xl pb-8 pt-6 slide-up-active"
           >
             <div className="mx-auto max-w-[420px] px-5">
-              <h2 className="text-lg tracking-tight font-bold mb-3">Pengaturan</h2>
-              <div className="flex flex-col gap-5">
+              <h2 className="text-lg tracking-tight font-bold mb-3">
+                Pengaturan
+              </h2>
+              <div className="flex flex-col gap-5 max-h-[60vh] overflow-auto -mr-5 pr-5">
                 <div className="flex flex-col gap-1">
                   <Label>Surat dan Ayat (Al-Qur{"'"}an)</Label>
                   <div className="flex flex-col gap-2">
