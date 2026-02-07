7dfd55fbcd6a3672dc0b74ff0a325f8d7413f4c4
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2026-02-07 11:50:59 +0700

    feat: add StaticHeroImage component and new hero image for mobile display

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - New `/lab/project-aidah` page
+- New hero image in homepage for mobile display
 
 ### Changed
 
---
+++ app/(main)/components/sections/HeroSection/StaticHeroImage.tsx
@@ -0,0 +1,15 @@
+import rahmatHero from "@/public/images/rahmat-hidayatullah-hero-static.png";
+import Image from "next/image";
+
+export function StaticHeroImage() {
+  return (
+    <div className="shrink-0 md:hidden block -mx-4">
+      <div className="h-full flex justify-center sm:justify-start">
+        <Image
+          src={rahmatHero}
+          alt="Foto Rahmat Hidayatullah dengan background semi transparan | hirahmat.dev"
+        />
+      </div>
+    </div>
+  );
+}
