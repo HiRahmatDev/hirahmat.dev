1d7f97720664db198c51b8b90e7c6706b2b91ac2
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2026-02-17 10:59:05 +0700

    chore: drop skeleton entrance animations in homepage
    - Update animate-pulse

--- app/(main)/components/sections/SelectedBlogSection/index.tsx
+++ app/(main)/components/sections/SelectedBlogSection/index.tsx
@@ -10,31 +10,28 @@ export async function SelectedBlogsSection() {
   return (
     <AnimatedWrapper>
       <div className="space-y-8">
-        <Header />
+        <div className="space-y-1 md:space-y-2">
+          <h2 className="selected-blogs-text-element invisible text-2xl/[36px] sm:text-4xl/[44px] -tracking-[.2px] font-bold blog-header">
+            Blog
+          </h2>
+          <p className="selected-blogs-text-element invisible text-base sm:text-lg max-w-[60ch] text-gray-600 blog-desc">
+            Catatan reflektif dan hal-hal yang saya pelajari di luar proyek.
+          </p>
+        </div>
         <SelectedBlogsList selectedBlogs={displayBlogs} hasMore={hasMore} />
       </div>
     </AnimatedWrapper>
   );
 }
 
-function Header() {
-  return (
-    <div className="space-y-1 md:space-y-2">
-      <h2 className="selected-blogs-text-element invisible text-2xl/[36px] sm:text-4xl/[44px] -tracking-[.2px] font-bold blog-header">
-        Blog
-      </h2>
-      <p className="selected-blogs-text-element invisible text-base sm:text-lg max-w-[60ch] text-gray-600 blog-desc">
-        Catatan reflektif dan hal-hal yang saya pelajari di luar proyek.
-      </p>
-    </div>
-  );
-}
-
 function Skeletons() {
   return (
-    <AnimatedWrapper>
+    <section className="container py-6 sm:py-8">
       <div className="space-y-8">
-        <Header />
+        <div className="space-y-1 md:space-y-2">
+          <div className="text-2xl/[36px] sm:text-4xl/[44px] h-lh w-50 bg-gray-100 animate-pulse rounded-2xl" />
+          <div className="text-base sm:text-lg h-lh w-100 bg-gray-100 animate-pulse rounded-2xl" />
+        </div>
         <div className="pb-6 pt-2 -mt-2 overflow-x-auto overflow-y-hidden -mx-6 sm:-mx-10 [&::-webkit-scrollbar]:hidden">
           <div className="flex gap-4 [&>div]:shrink-0">
             <div className="w-2 sm:w-6" />
@@ -43,7 +40,7 @@ function Skeletons() {
                 key={index}
                 className="selected-blog-card space-y-4 min-h-[307.25px]"
               >
-                <div className="shrink-0 w-84.5 aspect-video rounded-2xl bg-gray-100 border border-gray-100 animate-pulse" />
+                <div className="shrink-0 w-84.5 aspect-video rounded-2xl bg-gray-100 border border-gray-50 animate-pulse" />
                 <div className="space-y-1">
                   <div className="text-lg/normal h-lh w-full bg-gray-100 animate-pulse rounded-lg" />
                   <div className="text-lg/normal h-lh w-3/4 bg-gray-100 animate-pulse rounded-lg" />
@@ -53,7 +50,7 @@ function Skeletons() {
           </div>
         </div>
       </div>
-    </AnimatedWrapper>
+    </section>
   );
 }
 
--- app/(main)/components/sections/SelectedProjectsSection/index.tsx
+++ app/(main)/components/sections/SelectedProjectsSection/index.tsx
@@ -11,7 +11,14 @@ export async function SelectedProjectsSection() {
   return (
     <AnimatedWrapper>
       <div className="space-y-8">
-        <Header />
+        <div className="space-y-1 md:space-y-2">
+          <h2 className="selected-projects-text-element invisible text-2xl/[36px] sm:text-4xl/[44px] tracking-[-0.5px] sm:tracking-[-1px] font-bold">
+            Proyek <GreenText>Pilihan</GreenText>
+          </h2>
+          <p className="selected-projects-text-element invisible text-base sm:text-lg max-w-[60ch] -tracking-[.2px] text-gray-600">
+            Proyek pilihan dengan detail, interaksi, dan desain menarik.
+          </p>
+        </div>
         <SelectedProjectCards
           selectedProjects={displayProjects}
           hasMore={hasMore}
@@ -21,37 +28,27 @@ export async function SelectedProjectsSection() {
   );
 }
 
-function Header() {
-  return (
-    <div className="space-y-1 md:space-y-2">
-      <h2 className="selected-projects-text-element invisible text-2xl/[36px] sm:text-4xl/[44px] tracking-[-0.5px] sm:tracking-[-1px] font-bold">
-        Proyek <GreenText>Pilihan</GreenText>
-      </h2>
-      <p className="selected-projects-text-element invisible text-base sm:text-lg max-w-[60ch] -tracking-[.2px] text-gray-600">
-        Proyek pilihan dengan detail, interaksi, dan desain menarik.
-      </p>
-    </div>
-  );
-}
-
 function Skeletons() {
   return (
-    <AnimatedWrapper>
+    <section className="container py-8 sm:pt-6">
       <div className="space-y-8">
-        <Header />
+        <div className="space-y-1 md:space-y-2">
+          <div className="text-2xl/[36px] sm:text-4xl/[44px] h-lh w-50 bg-gray-100 animate-pulse rounded-2xl" />
+          <div className="text-base sm:text-lg h-lh w-100 bg-gray-100 animate-pulse rounded-2xl" />
+        </div>
         <div className="pb-10 [&::-webkit-scrollbar]:hidden overflow-x-auto -mx-6 sm:-mx-10">
           <div className="flex gap-4 [&>div]:shrink-0">
             <div className="w-2 sm:w-6" />
             {Array.from({ length: 2 }).map((_, index) => (
               <div
                 key={index}
-                className="selected-project-card w-109.75 h-54.25 rounded-3xl bg-gray-100 animate-pulse"
+                className="w-109.75 h-54.25 rounded-3xl bg-gray-100 animate-pulse"
               />
             ))}
           </div>
         </div>
       </div>
-    </AnimatedWrapper>
+    </section>
   );
 }
 
