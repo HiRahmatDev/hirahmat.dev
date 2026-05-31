9e243dc065cccbb4eeeae77c3ea34c4e10024153
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2026-05-31 14:47:23 +0700

    feat: add "/lab/test-mdx" for testing
    Setup NextJS MDX by installing "@mdx-js/loader", "@mdx-js/react",
    "@next/mdx", and "@types/mdx"; updating `next.config.ts`; creating
    `mdx-components.tsx`.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   images: {
     qualities: [10, 25, 50, 75, 90],
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX();
+
+export default withMDX(nextConfig);
